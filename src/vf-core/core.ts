f10cc30142c2411ce8ae54c302d5685bfb2f92c5
pospi <pospi@spadgos.com>
2020-07-22 20:21:56 +1000

    package to define UI-level constants for VF apps

---
+++ src/vf-core/core.ts
@@ -0,0 +1,25 @@
+/**
+ * Constants and other data structures useful in representing the VF spec.
+ *
+ * @package: VF UI core
+ * @since:   2020-07-22
+ */
+
+export const ACTION_IDS = [
+  'produce',
+  'use',
+  'consume',
+  'cite',
+  'work',
+  'pickup',
+  'dropoff',
+  'accept',
+  'modify',
+  'deliver-service',
+  'transfer',
+  'transfer-all-rights',
+  'transfer-custody',
+  'move',
+  'raise',
+  'lower',
+]
