62e4fec44bc2eb3aad66ff18b4afa8c4143e5598
pospi <pospi@spadgos.com>
2020-08-07 13:45:31 +1000

    date picker control WIP

--- src/views/offers/create-offer/date-input/DateInput.svelte
+++ src/views/offers/create-offer/date-input/DateInput.svelte
@@ -1,4 +1,19 @@
 <script>
+/**
+ * Date input with popup calendar picker & range functionality
+ *
+ * Usage:
+ *   <DateInput bind:value={someValueInParentComponent} />
+ *
+ * Todo:
+ * - backspace key to clear values
+ * - time inputs
+ * - think of a nicer way to toggle between single date & range mode
+ * - probably need to think about ranges in day mode (ie. a day selection encompasses the whole day)
+ *
+ * @package  ValueFlows UI
+ * @since    2020-08-03
+ */
 import Datepicker from 'praecox-datepicker'
 import ClickOutside from 'svelte-click-outside'
 import { format } from 'fecha'
@@ -7,25 +22,12 @@ export let selectRange = false
 export let placeholder = 'Select a date...'
 export let dateFormat = 'MMM Do, YYYY'
 
-let result
-let showDatePicker = false
-let pickerDone = false
+export let value = null // public API binding for accessing the input value
 
-// :TODO: make this smarter when start/end periods match (eg. remove year on second value if same year as start)
-function getDisplayResult () {
-  if (!result || (Array.isArray(result) && !result.length)) {
-    return ''
-  }
-  if (selectRange) {
-    if (result[0].start === result[1].end) {
-      return format(new Date(result[0].start), dateFormat)
-    } else {
-      return format(new Date(result[0].start), dateFormat) + ' - ' + format(new Date(result[1].end), dateFormat)
-    }
-  } else {
-    return format(new Date(result), dateFormat)
-  }
-}
+let showDatePicker = false
+let pickerResult
+let pickerDone = false
+let formattedResult
 
 function togglePicker () {
   showDatePicker = !showDatePicker
@@ -35,27 +37,64 @@ function closePicker () {
   showDatePicker = false
 }
 
-// keep input updated with currently selected value(s)
-$: formattedResult = result ? getDisplayResult() : ''
+// coerce values to Date objects
+function getDateValue (pickerVal) {
+  if (isEmptyValue(pickerVal)) {
+    return null
+  }
+console.warn('date value', pickerVal)
+  if (selectRange) {
+    return [new Date(pickerVal[0].start), new Date(pickerVal[1].end)]
+  } else {
+    return new Date(pickerVal)
+  }
+}
 
-// trigger hide of picker when done button is clicked
+// :TODO: make this smarter when start/end periods match (eg. remove year on second value if same year as start)
+function getDisplayResult (val) {
+  if (isEmptyValue(val)) {
+    return ''
+  }
+  if (selectRange) {
+    if (value[0] === value[1]) {
+      return format(value[0], dateFormat)
+    } else {
+      return format(value[0], dateFormat) + ' - ' + format(value[1], dateFormat)
+    }
+  } else {
+    return format(value, dateFormat)
+  }
+}
+
 $: {
+  if (pickerResult) {
+    // sync value to Date / Array version of selection
+    value = getDateValue(pickerResult)
+    // keep input updated with currently selected value(s)
+    formattedResult = getDisplayResult(value)
+  }
+  // trigger hide of picker when done button is clicked
   if (pickerDone) {
     pickerDone = false
     showDatePicker = false
   }
 }
+
+// internals
+function isEmptyValue (val) {
+  return !val || (Array.isArray(val) && !val.length)
+}
 </script>
 
 <div class="datepicker-wrap">
   <ClickOutside on:clickoutside={closePicker}>
-    <input type="text" readonly {placeholder} on:click={togglePicker} bind:value={formattedResult} />
+    <input type="text" {placeholder} on:click={togglePicker} bind:value={formattedResult} />
     {#if showDatePicker}
       <div class="picker-overlay">
         <Datepicker
           nowDate={new Date()}
           pickerRule={selectRange ? 'rangeChoice' : 'singleChoice'}
-          bind:pickerResult={result}
+          bind:pickerResult
           bind:pickerDone />
       </div>
     {/if}
