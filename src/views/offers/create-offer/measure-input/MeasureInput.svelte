b4c7c44038ddb31b85c9d773745b48e9a6380c3f
pospi <pospi@spadgos.com>
2020-08-12 20:18:34 +1000

    skeleton of measurement input type

---
+++ src/views/offers/create-offer/measure-input/MeasureInput.svelte
@@ -0,0 +1,22 @@
+<script>
+/**
+ * Form control for inputting measurements as "unit + value"
+ *
+ * @package  ValueFlows UI
+ * @since    2020-08-02
+ */
+
+export let numericalValue
+export let unit
+
+$: normalizedValue = { numericalValue, unit }
+</script>
+
+<div>
+  <input type="number" bind:value={numericalValue} />
+  <select bind:value={unit}>
+    <option value=""></option>
+    <option value="kg">kg</option>
+    <!-- :TODO: pull from backend -->
+  </select>
+</div>
