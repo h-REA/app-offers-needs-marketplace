9c7921b2af6290dfa18dd9a812b0fa7e922aa792
pospi <pospi@spadgos.com>
2020-08-12 20:19:20 +1000

    start breaking main form up into 'listing type' subforms

--- src/views/offers/create-offer/CreateOffer.svelte
+++ src/views/offers/create-offer/CreateOffer.svelte
@@ -14,10 +14,16 @@ let primaryIntent
 // bindings to child form control submission / validation actions
 let validateGiftIntent
 
+// top-level sub-states of the listing form
+const listingTypes = ['gift', 'need', 'offer', 'request']
+
 const { values, errors, dirty, validate, validity } = formup({
   schema: yup.object().shape({
     name: yup.string(),
     note: yup.string(),
+
+    // not VF fields, internal form state only
+    listingType: yup.string().oneOf(listingTypes),
   }),
   async onSubmit (data, context) {
     // trigger child form validation
@@ -29,40 +35,64 @@ const { values, errors, dirty, validate, validity } = formup({
     console.log('onSubmit', { data, context, primaryIntent })
   },
 })
+$values.listingType = 'gift'
 
 function updatePrimaryIntent (event) {
   primaryIntent = event.detail
 }
+
+// form labels (:TODO: move to i18n layer)
+const LISTING_TYPE_LABELS = {
+  gift: 'Gift something',
+  need: 'Ask for something',
+  offer: 'Make an offer',
+  request: 'Request an exchange',
+}
 </script>
 
 <form use:validate>
+  <h3>What will you do today?</h3>
+
   <p use:validity>
-    <label for="name">Name your offer</label>
+    {#each listingTypes as lType}
+    <label>
+      <input type=radio bind:group={$values.listingType} value={lType} />
+      {LISTING_TYPE_LABELS[lType]}
+    </label>
+    {/each}
+    <FieldError at="action" />
+  </p>
+
+  <p use:validity>
+    <label for="name">Name your listing</label>
     <input id="name" bind:value="{$values.name}" type="text" />
     <FieldError at="name" />
   </p>
 
   <p use:validity>
-    <label for="note">Notes</label>
-    <textarea id="note" bind:value="{$values.note}" />
+    <label for="note">Provide a description</label>
+    <textarea id="note" bind:value="{$values.note}" placeholder="be as detailed as you like..." />
     <FieldError at="note" />
   </p>
 
   <BindContextAgent let:contextAgent>
-    <ListGiftIntent {contextAgent} on:validated={updatePrimaryIntent} bind:validate={validateGiftIntent} />
+    {#if $values.listingType === 'gift'}
+      <ListGiftIntent {contextAgent} on:validated={updatePrimaryIntent} bind:validate={validateGiftIntent} />
+    {:else if $values.listingType === 'need'}
+    {:else if $values.listingType === 'offer'}
+    {:else if $values.listingType === 'request'}
+    {:else}
+    {/if}
   </BindContextAgent>
-
-  <!-- <label><input type="checkbox" on:click={onToggleRequestFields} /> Request an exchange -->
-
-  <!-- {#if requestVisible}
-  <section>
-    <header>Request details</header>
-      <IntentCreateForm reciprocal={true} />
-  </section>
-  {/if} -->
 
   <p>
     <button type="submit">Publish offer</button>
     <button type="reset">reset</button>
   </p>
 </form>
+
+<style>
+p, form {
+  text-align: left;
+}
+</style>
