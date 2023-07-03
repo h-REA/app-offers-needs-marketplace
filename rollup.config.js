66b2c71b5b2e41aefe9f1d24f292f896570afbea
pospi <pospi@spadgos.com>
2023-07-03 16:54:49 +1000

    Vite & Rollup configs for new build system

--- rollup.config.js
+++ rollup.config.js
@@ -1,83 +1,85 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import replace from '@rollup/plugin-replace'
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser'
-import typescript from '@rollup/plugin-typescript'
+import path from 'path';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import replace from "@rollup/plugin-replace";
 
-const svelteConfig = require('./svelte.config')
+import babel from "@rollup/plugin-babel";
+import html from "@web/rollup-plugin-html";
+import { importMetaAssets } from "@web/rollup-plugin-import-meta-assets";
+import { terser } from "rollup-plugin-terser";
+import typescript from '@rollup/plugin-typescript';
+import copy from 'rollup-plugin-copy';
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: 'src/main.ts',
+  input: "index.html",
   output: {
-    sourcemap: true,
-    format: 'iife',
-    name: 'app',
-    file: 'public/build/bundle.js'
+    entryFileNames: "[hash].js",
+    chunkFileNames: "[hash].js",
+    assetFileNames: "[hash][extname]",
+    format: "es",
+    dir: "dist",
+    sourcemap: !production,
   },
-  plugins: [
-    svelte(svelteConfig),
-
-    replace({
-      // Configuration env vars passthrough
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'process.env.REACT_APP_HC_CONN_URL': JSON.stringify(process.env.REACT_APP_HC_CONN_URL),
-
-      // GraphQL client type to use for development.
-      // Possible values:
-      //   - 'mock' to use a lightweight mocked datasource
-      //   - 'holochain' to connect to a Holochain websocket running Holo-REA at process.env.REACT_APP_HC_CONN_URL
-      // Planned:
-      //   - 'commonspub'
-      //   - 'scuttlebutt'
-      __GRAPHQL_CLIENT_MODULE__: '@vf-ui/graphql-client-holochain',
-    }),
-
-    // If you have external dependencies installed from
-    // npm, you'll most likely need these plugins. In
-    // some cases you'll need additional configuration -
-    // consult the documentation for details:
-    // https://github.com/rollup/plugins/tree/master/packages/commonjs
-    resolve({
-      browser: true,
-      dedupe: ['svelte', 'graphql']
-    }),
-    commonjs(),
-    typescript({ sourceMap: !production }),
-
-    // In dev mode, call `npm run start` once
-    // the bundle has been generated
-    !production && serve(),
-
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
-    !production && livereload('public'),
-
-    // If we're building for production (npm run build
-    // instead of npm run dev), minify
-    production && terser()
-  ],
   watch: {
-    clearScreen: false
-  }
+    clearScreen: false,
+  },
+
+  plugins: [
+    /** Enable using HTML as rollup entrypoint */
+    html({
+      minify: true,
+    }),
+    /** Resolve bare module imports */
+    nodeResolve({
+      browser: true,
+      preferBuiltins: false,
+    }),
+    replace({
+      "process.env.NODE_ENV": '"production"',
+      "process.env.ENV": `"${process.env.ENV}"`,
+      "process.env.HC_PORT": `undefined`,
+      "process.env.ADMIN_PORT": `undefined`,
+      "process.env.REACT_APP_HC_CONN_URL": `undefined`,
+      "process.env.REACT_APP_HC_ADMIN_CONN_URL": `undefined`,
+      "process.env.REACT_APP_HC_APP_ID": `undefined`,
+    }),
+    commonjs({}),
+    typescript({
+      sourceMap: !production,
+      inlineSources: !production,
+    }),
+    /** Minify JS */
+    terser(),
+    /** Bundle assets references via import.meta.url */
+    importMetaAssets(),
+    /** Compile JS to a lower language target */
+    babel({
+      exclude: /node_modules/,
+
+      babelHelpers: "bundled",
+      presets: [
+        [
+          require.resolve("@babel/preset-env"),
+          {
+            targets: ['defaults', 'not IE 11', 'safari >13', 'not op_mini all', 'last 3 Chrome versions'],
+            modules: false,
+            bugfixes: true,
+          },
+        ],
+      ],
+      plugins: [],
+    }),
+    // Copy Shoelace assets to dist/shoelace
+    copy({
+      copyOnce: true,
+      targets: [
+        {
+          src: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets'),
+          dest: path.resolve(__dirname, 'dist/shoelace')
+        }
+      ]
+    })
+  ],
 };
-
-function serve() {
-  let started = false;
-
-  return {
-    writeBundle() {
-      if (!started) {
-        started = true;
-
-        require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-          stdio: ['ignore', 'inherit', 'inherit'],
-          shell: true
-        });
-      }
-    }
-  };
-}
